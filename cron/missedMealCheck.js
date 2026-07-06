ce6c6b595c2d48f55c83278000c572019b673e6e
s1mpleNova <shahnaman632002@gmail.com>
2026-07-06 21:12:03 +0530

    mailer

--- cron/missedMealCheck.js
+++ cron/missedMealCheck.js
@@ -8,6 +8,7 @@ const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 465,
     secure: true,
+    family: 4,
     auth: {
         user: process.env.GMAIL_USER,
         pass: process.env.GMAIL_APP_PASSWORD,
